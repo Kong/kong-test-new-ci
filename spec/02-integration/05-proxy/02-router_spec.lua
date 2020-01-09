max8899/kong

feat(router) add routes.path_handling attribute
This change introduces a new Routes attribute called path_handling.

It complements #5216 and #5227 by making it possible to select the
behavior the router will have when combining the Service Path, the Route
Path and the Request path into a single path sent to the upstream.

For now `path_handling` accepts two values:

* "v0" makes paths in the router behave like in Kong 0.x (like they did
  before #3780)
* "v1" makes paths behave like in Kong 1.x (like they did after #3780)

Notice that in order to add this new attribute a lot of tests needed to
be duplicated. The cases were v0 differs from v1 have been marked with
`-- !` for easy identification.

spec/02-integration/05-proxy/02-router_spec.lua
@@ -1473,6 +1473,7 @@ for _, strategy in helpers.each_strategy() do
           for i, test in ipairs(path_handling_tests) do
             routes[i] = {
               strip_path   = test.strip_path,
+              path_handling = test.path_handling,
               paths        = test.route_path and { test.route_path } or nil,
               hosts        = { "localbin-" .. i .. ".com" },
               service = {
@@ -1492,16 +1493,16 @@ for _, strategy in helpers.each_strategy() do
         end)
 
         for i, test in ipairs(path_handling_tests) do
-          local config = string.format("route.strip_path=%s", test.strip_path and "on" or "off")
-
-          local description
+          local strip = test.strip_path and "on" or "off"
+          local route_uri_or_host
           if test.route_path then
-            description = string.format("(%d) (%s) %s with uri %s when requesting %s",
-                                        i, config, test.upstream_path, test.route_path, test.request_path)
+            route_uri_or_host = "uri " .. test.route_path
           else
-            description = string.format("(%d) (%s) %s with host %s when requesting %s",
-                                        i, config, test.upstream_path, "localbin-" .. i .. ".com", test.request_path)
+            route_uri_or_host = "host localbin-" .. i .. ".com"
           end
+
+          local description = string.format("(%d) %s with %s, strip = %s, %s when requesting %s",
+            i, test.upstream_path, route_uri_or_host, strip, test.path_handling, test.request_path)
 
           it(description, function()
             local res = assert(proxy_client:get(test.request_path, {
@@ -1530,6 +1531,7 @@ for _, strategy in helpers.each_strategy() do
             routes[i] = {
               strip_path   = test.strip_path,
               paths        = test.route_path and { make_a_regex(test.route_path) } or nil,
+              path_handling = test.path_handling,
               hosts        = { "localbin-" .. i .. ".com" },
               service = {
                 name = "make_regex_" .. i,
@@ -1548,10 +1550,10 @@ for _, strategy in helpers.each_strategy() do
         for i, test in ipairs(path_handling_tests) do
           if test.route_path then  -- skip if hostbased match
 
-            local config = string.format("route.strip_path=%s", test.strip_path and "on" or "off")
+            local strip = test.strip_path and "on" or "off"
 
-            local description = string.format("(%d) (%s) %s with uri %s when requesting %s",
-              i, config, test.upstream_path, make_a_regex(test.route_path), test.request_path)
+            local description = string.format("(%d) %s with uri %s, strip = %s, %s when requesting %s",
+              i, test.upstream_path, make_a_regex(test.route_path), strip, test.path_handling, test.request_path)
 
             it(description, function()
               local res = assert(proxy_client:get(test.request_path, {

Kong/kong-test-new-ci

spec/02-integration/05-proxy/02-router_spec.lua
@@ -1530,6 +1530,7 @@ for _, strategy in helpers.each_strategy() do
           for i, test in ipairs(path_handling_tests) do
             routes[i] = {
               strip_path   = test.strip_path,
+              path_handling = test.path_handling,
               paths        = test.route_path and { test.route_path } or nil,
               hosts        = { "localbin-" .. i .. ".com" },
               service = {
@@ -1549,16 +1550,16 @@ for _, strategy in helpers.each_strategy() do
         end)
 
         for i, test in ipairs(path_handling_tests) do
-          local config = string.format("route.strip_path=%s", test.strip_path and "on" or "off")
-
-          local description
+          local strip = test.strip_path and "on" or "off"
+          local route_uri_or_host
           if test.route_path then
-            description = string.format("(%d) (%s) %s with uri %s when requesting %s",
-                                        i, config, test.service_path, test.route_path, test.request_path)
+            route_uri_or_host = "uri " .. test.route_path
           else
-            description = string.format("(%d) (%s) %s with host %s when requesting %s",
-                                        i, config, test.service_path, "localbin-" .. i .. ".com", test.request_path)
+            route_uri_or_host = "host localbin-" .. i .. ".com"
           end
+
+          local description = string.format("(%d) %s with %s, strip = %s, %s when requesting %s",
+            i, test.service_path, route_uri_or_host, strip, test.path_handling, test.request_path)
 
           it(description, function()
             local res = assert(proxy_client:get(test.request_path, {
@@ -1587,6 +1588,7 @@ for _, strategy in helpers.each_strategy() do
             routes[i] = {
               strip_path   = test.strip_path,
               paths        = test.route_path and { make_a_regex(test.route_path) } or nil,
+              path_handling = test.path_handling,
               hosts        = { "localbin-" .. i .. ".com" },
               service = {
                 name = "make_regex_" .. i,
@@ -1605,10 +1607,10 @@ for _, strategy in helpers.each_strategy() do
         for i, test in ipairs(path_handling_tests) do
           if test.route_path then  -- skip if hostbased match
 
-            local config = string.format("route.strip_path=%s", test.strip_path and "on" or "off")
+            local strip = test.strip_path and "on" or "off"
 
-            local description = string.format("(%d) (%s) %s with uri %s when requesting %s",
-              i, config, test.service_path, make_a_regex(test.route_path), test.request_path)
+            local description = string.format("(%d) %s with uri %s, strip = %s, %s when requesting %s",
+              i, test.service_path, make_a_regex(test.route_path), strip, test.path_handling, test.request_path)
 
             it(description, function()
               local res = assert(proxy_client:get(test.request_path, {